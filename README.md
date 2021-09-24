575174046ea08c0c93386e27e46805294c265b98
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2021-09-24 19:07:13 -0300

    Implementado Primeira Versao sem Clean

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # desafio-stefanini
 Repositório com o desafio proposto.
+
+* serverless config credentials -o --provider aws --key=USER_KEY --secret USER_SECRET
+
+
--- handler.js
+++ handler.js
@@ -1,18 +1,96 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+require('dotenv').config({ path: './variables.env' });
+const connectToDatabase = require('./db');
+const User = require('./models/User');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+module.exports.create = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      User.create(JSON.parse(event.body))
+        .then(user => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(user)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not create the user.'
+        }));
+    });
+};
+
+module.exports.getOne = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      User.findById(event.pathParameters.id)
+        .then(user => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(user)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the user.'
+        }));
+    });
+};
+
+module.exports.getAll = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      User.find()
+        .then(users => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(users)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the user.'
+        }))
+    });
+};
+
+module.exports.update = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      User.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+        .then(user => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(user)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the user.'
+        }));
+    });
+};
+
+module.exports.delete = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      User.findByIdAndRemove(event.pathParameters.id)
+        .then(user => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Removed user with id: ' + user._id, user: user })
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the user.'
+        }));
+    });
 };
